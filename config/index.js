3a8857c2d7417efc8548476583085c22d868e7cf
Ben <brbryant2639@gmail.com>
2022-03-04 11:08:44 -0600

    UPDATE: added cors, helmet, and json parser

--- config/index.js
+++ config/index.js
@@ -5,6 +5,10 @@ const envFound = dotenv.config();
 if (!envFound) throw new Error("Failed to load env variables");
 
 module.exports = {
-    env: process.env.NODE_ENV || "development",
+  env: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT) || 5000,
+  glot: {
+    run_url: process.env.GLOT_RUN_URL,
+    token: process.env.GLOT_TOKEN,
+  },
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const http = require("http");
-const socket = require("socket.io");
+const fetch = require("isomorphic-fetch");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const { Server } = require("socket.io");
 
 const config = require("../config");
 const { getLangExt } = require("./utils/code");
@@ -9,7 +13,9 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = socket(server);
+const allowedOrigin =
+  config.env == "development" ? "http://localhost:3000" : "#";
+const io = new Server(server, { cors: { origin: allowedOrigin } });
 
 io.on("connection", (socket) => {
   console.log("socket established " + new Date().toString());
@@ -44,14 +50,18 @@ if (config.env == "development") {
   app.use(require("morgan")("dev"));
 }
 
-app.post("/api/run", async (req, res) => {
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/code/run", async (req, res) => {
   const { name, content, language } = req.body;
   console.log(req.body);
   let langExt = getLangExt(language);
 
-  let result = await fetch(`${GLOT_URL}${language}/latest`, {
+  let result = await fetch(`${config.glot.run_url}${language}/latest`, {
     method: "POST",
-    headers: { Authorization: `Token ${GLOT_TOKEN}` },
+    headers: { Authorization: `Token ${config.glot.token}` },
     body: JSON.stringify({ files: [{ name: name + langExt, content }] }),
   });
   let data = await result.json();
