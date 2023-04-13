211614c5e42d0fb31d83fc88523fcac1bf6a58bc
Ben Bryant <brbryant@uabmc.edu>
2023-04-13 14:00:56 -0500

    UPDATE: trimmed for redesign

--- config/index.js
+++ config/index.js
@@ -7,10 +7,6 @@ if (!envFound) throw new Error("Failed to load env variables");
 module.exports = {
   env: process.env.NODE_ENV || "development",
   port: parseInt(process.env.PORT) || 5000,
-  glot: {
-    run_url: process.env.GLOT_RUN_URL,
-    token: process.env.GLOT_TOKEN,
-  },
   mongo: {
     uri: process.env.MONGODB_URI,
     db: process.env.MONGODB,
--- src/server.js
+++ src/server.js
@@ -2,54 +2,12 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const helmet = require("helmet");
-const { MongoClient } = require("mongodb");
-const { Server } = require("socket.io");
 const config = require("../config");
 const routes = require("./routes");
 
 const app = express();
 
-console.log(config.mongo.uri)
-
-const client = new MongoClient(config.mongo.uri);
-
 const server = http.createServer(app);
-
-const allowedOrigin =
-  config.env == "development" ? "http://localhost:3000" : "*";
-const io = new Server(server, { cors: { origin: allowedOrigin } });
-
-io.on("connection", (socket) => {
-  console.log("socket established " + new Date().toString());
-
-  socket.on("join-room", (data) => {
-    console.log("User is joining...", data);
-    const { roomId, user } = data;
-
-    socket.join(roomId);
-
-    socket.to(roomId).emit("new-user-connect", data);
-
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-disconnected", user);
-    });
-
-    socket.on("code-change", async (code) => {
-      let result = await client
-        .db(config.mongo.db)
-        .collection(config.mongo.collection)
-        .updateOne({ roomId }, { $set: { code } });
-
-      console.log(result);
-
-      socket.to(roomId).emit("update-code", code);
-    });
-
-    socket.on("run-code", (data) => {
-      socket.to(roomId).emit("run-code", data);
-    });
-  });
-});
 
 if (config.env == "development") {
   app.use(require("morgan")("dev"));
@@ -59,10 +17,6 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.client = client;
-  next();
-});
 app.use("/api", routes);
 
 // Default Display for any request not matching /api...
@@ -76,11 +30,4 @@ app.use((req, res, next) => {
 
 server.listen(config.port, async () => {
   console.log(`✅ Server running on port ${config.port}`);
-
-  try {
-    await client.connect();
-    console.log(`✅ Connected to mongodb`);
-  } catch (e) {
-    console.error(e);
-  }
 });
